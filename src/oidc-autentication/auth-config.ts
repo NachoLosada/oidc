79dbceaf3fae6e509094880946159910c7ccec87
Cristian Payet <payet91@gmail.com>
2024-08-28 13:23:24 +0200

    feat: changes

--- src/oidc-autentication/auth-config.ts
+++ src/oidc-autentication/auth-config.ts
@@ -1,26 +1,25 @@
 import { UserManagerSettings } from "oidc-client-ts";
 
-// const oidcConfig: UserManagerSettings = {
-//   authority: "https://preproduccio.login.siep.justicia.gencat.cat",
-//   client_id: "epvt-extranet",
-//   redirect_uri: "https://preproduccio.login.siep.justicia.gencat.cat/",
-//   post_logout_redirect_uri: "http://localhost:5173/",
-//   redirectMethod: "replace",
-//   response_type: "code",
-//   scope: "openid",
-//   filterProtocolClaims: true,
-//   loadUserInfo: true,
-// };
-
 const oidcConfig: UserManagerSettings = {
-  authority: "https://demo.duendesoftware.com",
-  client_id: "interactive.public",
-  redirect_uri: "http://localhost:5173/",
+  authority: "https://preproduccio.login.siep.justicia.gencat.cat",
+  client_id: "vtvdv",
+  redirect_uri: "http://localhost:5173",
   redirectMethod: "replace",
   response_type: "code",
   scope: "openid",
   filterProtocolClaims: true,
   loadUserInfo: true,
 };
+
+// const oidcConfig: UserManagerSettings = {
+//   authority: "https://demo.duendesoftware.com",
+//   client_id: "interactive.public",
+//   redirect_uri: "http://localhost:5173/",
+//   redirectMethod: "replace",
+//   response_type: "code",
+//   scope: "openid",
+//   filterProtocolClaims: true,
+//   loadUserInfo: true,
+// };
 
 export default oidcConfig;
